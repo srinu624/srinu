eb56724b81406fbe64ebb5a6422ae3c655551e88
srinu624 <smajji162@gmail.com>
2023-08-09 14:35:33 +0530

    my commit

---
+++ addition.py
@@ -0,0 +1,29 @@
+# # python program to add two numbers with two inputs
+# num1=21
+# num2=17
+# sum = num1 + num2
+# print("Sum of", num1, "and", num2 , "is", sum)
+
+
+# # python program to add two numbers with user inputs
+# num1 = input("First number: ")
+# num2 = input("\nsecond number: ")
+# sum = float(num1) + float(num2)
+# # print("Sum of {0} and {1} is {2};" .format(num1, num2, sum))
+# print("Sum of", num1, "and", num2 , "is", sum)
+
+
+# python program using add function and return the result
+def add(a,b):
+  return a+b
+ 
+#initializing the variables
+num1 = 10
+num2 = 5
+ 
+#function calling and store the result into sum_of_twonumbers
+sum_of_twonumbers = add(num1,num2)
+ 
+#To print the result
+print("Sum of {0} and {1} is {2};" .format(num1,   num2, sum_of_twonumbers))
+                        
